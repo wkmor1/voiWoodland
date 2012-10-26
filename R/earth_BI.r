56d35931700cb7d7afcf9e8c33a18c36cf005a57
wkmor1 <wkmor1@gmail.com>
2012-10-26 11:01:54 -0400

    changes and bug fixes to earth_BI.r

--- R/earth_BI.r
+++ R/earth_BI.r
@@ -1,55 +1,40 @@
-earth_BI <- function(n_boot, BIModelOutput, inputs, verbose=FALSE) {
-  
-  attach(inputs)
-  
-  earth_BI_obj <- vector('list', length(inputs))
-  names(earth_BI_obj) <- names(inputs)
-  
-  for (i in names(earth_BI_obj)) {
-   
-    earth_BI_obj_i <- vector('list', 3)
-    names(earth_BI_obj_i) <- c('LDW', 'HDW', 'MAT')
-      
-    for (j in names(earth_BI_obj_i)) {
+earth_BI <- function(n_boot, BIModelOutput, inputs, dir, verbose=FALSE) {
     
-      earth_BI_obj_ij <- vector('list', 15)
+  for (i in names(inputs)) {
       
-      for (k in seq_along(earth_BI_obj_ij) ) {
+    for (j in c('LDW', 'HDW', 'MAT')) {
+          
+      for (k in 1:15) {
       
         earth_BI_obj_ijk <- vector('list', 2)
         
         BIModelOutputSS <- subset(BIModelOutput, CTAbbr == i & SSAbbr == j & Timestep == k * 10)
-        BIModelOutputSS <- merge(BIModelOutputSS, get(sprintf('%sParams', i)), by.x=1, by.y=0)
+        BIModelOutputSS <- merge(BIModelOutputSS, inputs[[i]], by.x=1, by.y=0)
         
         if(verbose) cat('Model: ')
         
-        earth_BI_obj_ijk[[1]] <- earth(Area ~ ., data=BIModelOutputSS[-1:-4], degree=3)
+        earth_BI_obj_ijk[[1]] <- earth(qlogis(((Area / (1000 / 3)) * .998) + .001) ~ ., 
+          data=BIModelOutputSS[-1:-4], degree=3)
         
         earth_BI_obj_ijk[[1]]$fitted.values <- NA
         
         if(verbose) cat(sprintf('%s %s %s \n', i, j, k))
         
         earth_BI_obj_ijk[[2]] <- mclapply(integer(n_boot), eval.parent(substitute(function(...) {
-          x <- earth(Area ~ ., 
+          x <- earth(qlogis(((Area / (1000 / 3)) * .998) + .001) ~ ., 
             data=BIModelOutputSS[sample(seq_len(nrow(BIModelOutputSS)), replace=TRUE), -1:-4], 
             degree=3)
           x$fitted.values <- NA
           x
           })), mc.cores=detectCores() / ceiling(n_boot / detectCores()))
         
-        earth_BI_obj_ij[[k]] <- earth_BI_obj_ijk
+        save('earth_BI_obj_ijk', file=sprintf('%s/%s_%s_%s.rda', dir, i, j, k))
         
       }
-
-      earth_BI_obj_i[[j]] <- earth_BI_obj_ij
     
     }
-    
-    earth_BI_obj[[i]] <- earth_BI_obj_i
-  
+      
   }
-  
-  return(earth_BI_obj)
   
 }  
 
