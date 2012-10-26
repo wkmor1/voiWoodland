d25e30f6ec92f8075ff6f376bfac3b13ea57550d
wkmor1 <wkmor1@gmail.com>
2012-10-26 11:04:28 -0400

    new function pre_poseterior for creating sets of preposterior distributions of parameters

---
+++ R/pre_posterior.r
@@ -0,0 +1,31 @@
+pre_posterior <- function(x, n, size) {
+  
+  eta <- sum(x == 0) / length(x)
+  
+  if(var(x[x != 0])) shape <- mmbeta(x[x != 0]) else shape <- NULL
+  
+  p <- unname(sample(x, n, replace=TRUE))
+  
+  update <- function(phi, x, size, eta, shape) {
+    m <- rbinom(1, size, phi)
+    
+    if(m < 1 && eta) {
+      x.star <- unname(x)
+      if(var(x[x != 0])) x.star[x.star != 0] <- rbeta(length(x) - sum(x == 0), shape[1], shape[2] + size)
+    } else {
+      if(var(x[x != 0])) {
+        x.star <- unname(rbeta(length(x), shape[1] + m, shape[2] + (size - m)))
+      } else {
+        x.star <- rep(x[x != 0], length.out=length(x))
+      }  
+    }
+    attr(x.star, 'm') <- m
+    return(x.star)
+  }
+  
+  ans <- lapply(p, function(y) update(phi=y, x=x, size=size, eta=eta, shape=shape))
+  attr(ans, 'probability') <- p
+  return(ans)
+  
+}
+
