58e6e1c004872198473ea9db170089553c30b4d4
wkmor1 <wkmor1@gmail.com>
2012-10-30 16:56:59 -0400

    fixed update of parameters that can either be zero or one. Experiments can either result in successes or failures. Update rules out p = opposite in each case.

--- R/pre_posterior.r
+++ R/pre_posterior.r
@@ -11,8 +11,10 @@ pre_posterior <- function(x, n, size) {
     
     if(m < 1 && eta) {
       x.star <- unname(x)
+      if(diff(range(x)) == 1) x.star <- rep(0, length(x))
       if(var(x[x != 0])) x.star[x.star != 0] <- rbeta(length(x) - sum(x == 0), shape[1], shape[2] + size)
     } else {
+      if(diff(range(x)) == 1) x.star <- rep(1, length(x))
       if(var(x[x != 0])) {
         x.star <- unname(rbeta(length(x), shape[1] + m, shape[2] + (size - m)))
       } else {
