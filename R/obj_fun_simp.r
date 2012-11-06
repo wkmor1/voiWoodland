8386c7424f820066faf30d428b61d4a5d70ed451
wkmor1 <wkmor1@gmail.com>
2012-11-06 17:27:19 -0500

    minor changes to obj_fun_simp.r format

--- R/obj_fun_simp.r
+++ R/obj_fun_simp.r
@@ -1,26 +1,37 @@
-obj_fun_simp <- function(x, sens_BI, BI_output, upd.manage=c('BIW', 'BIMan', 'BIEco', 'NONE'), k) {
+obj_fun_simp <- function(x, sens_pp_BI, BI_output, 
+  upd.manage=c('BIW', 'BIMan', 'BIEco', 'NONE'), k) {
+  
   A <- x[1]
   B <- x[2]
   
   upd.manage <- match.arg(upd.manage)
   
   BIW <- switch(upd.manage,
-    BIW = unlist(sens_BI[[k]][['MAT']][[15]]),
-    BIMan = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIW' & SSAbbr == 'MAT', select=Area)),
-    BIEco = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIW' & SSAbbr == 'MAT', select=Area)),
-    NONE = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIW' & SSAbbr == 'MAT', select=Area)))
+    BIW = unlist(sens_pp_BI[[k]][['MAT']][[15]]),
+    BIMan = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIW' & SSAbbr == 'MAT', select=Area)),
+    BIEco = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIW' & SSAbbr == 'MAT', select=Area)),
+    NONE = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIW' & SSAbbr == 'MAT', select=Area)))
   
   BIMan <- switch(upd.manage,
-    BIMan = unlist(sens_BI[[k]][['MAT']][[15]]),
-    BIW = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIMan' & SSAbbr == 'MAT', select=Area)),
-    BIEco = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIMan' & SSAbbr == 'MAT', select=Area)),
-    NONE = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIMan' & SSAbbr == 'MAT', select=Area)))
+    BIMan = unlist(sens_pp_BI[[k]][['MAT']][[15]]),
+    BIW = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIMan' & SSAbbr == 'MAT', select=Area)),
+    BIEco = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIMan' & SSAbbr == 'MAT', select=Area)),
+    NONE = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIMan' & SSAbbr == 'MAT', select=Area)))
   
   BIEco <- switch(upd.manage,
-    BIEco = unlist(sens_BI[[k]][['MAT']][[15]]),
-    BIW = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIEco' & SSAbbr == 'MAT', select=Area)),
-    BIMan = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIEco' & SSAbbr == 'MAT', select=Area)),
-    NONE = unlist(subset(BI_output, Timestep == 150 & CTAbbr == 'BIEco' & SSAbbr == 'MAT', select=Area)))
+    BIEco = unlist(sens_pp_BI[[k]][['MAT']][[15]]),
+    BIW = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIEco' & SSAbbr == 'MAT', select=Area)),
+    BIMan = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIEco' & SSAbbr == 'MAT', select=Area)),
+    NONE = unlist(subset(BI_output, 
+      Timestep == 150 & CTAbbr == 'BIEco' & SSAbbr == 'MAT', select=Area)))
   
   mean(A * BIW + B * BIMan + (1 - A - B) * BIEco) 
   
