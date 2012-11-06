f491700c71772b99207af6e472d7707a14bb1b5a
wkmor1 <wkmor1@gmail.com>
2012-11-06 17:28:07 -0500

    added output data argument to evsi function

--- R/evsi_BI.r
+++ R/evsi_BI.r
@@ -1,4 +1,4 @@
-evsi_BI <- function(sens_BI, manage) {
+evsi_BI <- function(sens_pp_BI, manage, BI_output) {
 
   init <- c(1/3, 1/3)
 
@@ -14,7 +14,7 @@ evsi_BI <- function(sens_BI, manage) {
   max_k <- mclapply(1:10, function(k) {
       constrOptim(init, 
         function(x) {
-          obj_fun_simp(x, sens_BI=sens_BI, 
+          obj_fun_simp(x, sens_pp_BI=sens_pp_BI, 
             BI_output=BI_output, upd.manage=manage, k)},
         NULL, ui, ci, control=list(fnscale=-1))
       }, mc.cores=detectCores())
