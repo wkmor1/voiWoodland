39b59e3cd95be6c3595e147191e20b3e1dd4108e
wkmor1 <wkmor1@gmail.com>
2012-11-15 16:13:48 -0500

    New function evpi_BI added to calculate expected value of perfect information. As a result the function calc_evsi has been renamed calc_evi to reflect that it can calculate either EVPI or EVSI

--- R/calc_evsi_BI.r
+++
@@ -1,4 +0,0 @@
-calc_evsi_BI <- function(x) {
-   mean(sapply(1:10, function(y) x[[2]][[y]]$value)) -
-     x[[1]]$value
- }
--- R/evpi_BI.r
+++ R/evpi_BI.r
@@ -1,4 +1,4 @@
-evsi_BI <- function(sens_pp_BI, manage, BI_output) {
+evpi_BI <- function(BI_output, n) {
 
   init <- c(1/3, 1/3)
 
@@ -11,11 +11,11 @@ evsi_BI <- function(sens_pp_BI, manage, BI_output) {
       BI_output=BI_output, upd.manage='NONE')},
     NULL, ui, ci, control=list(fnscale=-1))
 
-  max_k <- mclapply(1:10, function(k) {
+  max_k <- mclapply(levels(BI_output$Project)[sample(1:375, n)], function(k) {
       constrOptim(init, 
         function(x) {
-          obj_fun_simp(x, sens_pp_BI=sens_pp_BI, 
-            BI_output=BI_output, upd.manage=manage, k)},
+          obj_fun_simp(x, 
+            BI_output=subset(BI_output, Project == k), upd.manage='NONE')},
         NULL, ui, ci, control=list(fnscale=-1))
       }, mc.cores=5)
  
