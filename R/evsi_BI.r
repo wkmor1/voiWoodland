dc1ef974f025bb13508518865168b552d04555f9
wkmor1 <wkmor1@gmail.com>
2013-03-25 15:23:29 -0400

    changed parameters, inits, objective function and output data for evsi_BI.r

--- R/evsi_BI.r
+++ R/evsi_BI.r
@@ -1,21 +1,22 @@
-evsi_BI <- function(sens_pp_BI, manage, BI_output) {
-
-  init <- c(1/3, 1/3)
+evsi_BI <- function(BIW=NULL, BIMan=NULL, BIEco=NULL, BI_output) {
+  
+  init <- c(1/3, .1)
 
   ui <- rbind(c(1, 0), c(0, 1), c(-1, 0), c(0, -1), c(-1, -1))
-  ci <- c(0, 0, -1, -1, -1)
+  ci <- c(0, 0, -1, -.2, -1)
 
   e_max <- constrOptim(init, 
     function(x) {
-      obj_fun_simp(x,
-      BI_output=BI_output, upd.manage='NONE')},
+      obj_fun_simp(x, BIW=NULL, BIMan=NULL, BIEco=NULL, BI_output=BI_output)},
     NULL, ui, ci, control=list(fnscale=-1))
 
-  max_k <- mclapply(seq_along(sens_pp_BI), function(k) {
+  max_k <- mclapply(seq_len(max(length(BIW), length(BIMan), length(BIEco))), 
+    function(k) {
       constrOptim(init, 
         function(x) {
-          obj_fun_simp(x, sens_pp_BI=sens_pp_BI, 
-            BI_output=BI_output, upd.manage=manage, k)},
+          obj_fun_simp(x,
+            BI_output=BI_output,
+            BIW=BIW, BIMan=BIMan, BIEco=BIEco, k)},
         NULL, ui, ci, control=list(fnscale=-1))
       }, mc.cores=5)
  
