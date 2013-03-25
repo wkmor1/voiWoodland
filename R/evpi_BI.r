7a3cc745600608a51d9f15bad3b0bae2dea89caa
wkmor1 <wkmor1@gmail.com>
2013-03-25 15:22:42 -0400

    changed inits, obj function and output data for evpi_BI.r

--- R/evpi_BI.r
+++ R/evpi_BI.r
@@ -1,21 +1,20 @@
 evpi_BI <- function(BI_output, n) {
 
-  init <- c(1/3, 1/3)
+  init <- c(1/3, .1)
 
   ui <- rbind(c(1, 0), c(0, 1), c(-1, 0), c(0, -1), c(-1, -1))
-  ci <- c(0, 0, -1, -1, -1)
+  ci <- c(0, 0, -1, -.2, -1)
 
   e_max <- constrOptim(init, 
     function(x) {
-      obj_fun_simp(x,
-      BI_output=BI_output, upd.manage='NONE')},
+      obj_fun_simp(x, BI_output=BI_output)},
     NULL, ui, ci, control=list(fnscale=-1))
 
-  max_k <- mclapply(levels(BI_output$Project)[sample(1:375, n)], function(k) {
+  max_k <- mclapply(sample(1:375, n), function(k) {
       constrOptim(init, 
         function(x) {
           obj_fun_simp(x, 
-            BI_output=subset(BI_output, Project == k), upd.manage='NONE')},
+            BI_output=rapply(BI_output, function(y) y[k, ], how='replace'))},
         NULL, ui, ci, control=list(fnscale=-1))
       }, mc.cores=5)
  
